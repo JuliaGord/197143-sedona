68a86bc4b1163d65a4269cb8f12830ed39f63b92
JuliaGord <jg0311@bk.ru>
2016-09-06 18:23:51 +0300

    Добавление анимации и интерактивной карты №1

---
+++ js/popup.js
@@ -0,0 +1,22 @@
+var link = document.querySelector(".search-title-link");
+var popup = document.querySelector(".search-form");
+var form = popup.querySelector("form");
+var datein = popup.querySelector("[name=datein]");
+var dateout = popup.querySelector("[name=dateout]");
+link.addEventListener("click", function(event) {
+ event.preventDefault();
+ popup.classList.toggle("search-form-show");
+});
+
+form.addEventListener("submit", function(event) {
+        if (!datein.value || !dateout.value) {
+          event.preventDefault();
+          popup.offsetWidth = popup.offsetWidth;
+          popup.classList.add("search-form-error");
+        }
+       else {
+           if (popup.classList.contains("search-form-show")) {
+         popup.classList.remove("search-form-error");
+           }
+      }
+      });
